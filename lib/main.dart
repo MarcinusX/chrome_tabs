9a75f16663daa904f3438dd68251250b8a5d7a54
Marcin Szalek <marcin.szalek1@gmail.com>
2018-06-21 14:00:38 +0200

    Changes to scroll behavior

--- lib/main.dart
+++ lib/main.dart
@@ -39,41 +39,56 @@ class _MyHomePageState extends State<MyHomePage> {
         title: new Text(widget.title),
       ),
       body: new GestureDetector(
+        onVerticalDragStart: (DragStartDetails details) {
+          print("Start: ${details.globalPosition.dy}");
+        },
         onVerticalDragUpdate: (DragUpdateDetails details) {
-          print(details);
+//          print(details.delta.dy);
           setState(() {
             offset += details.delta.dy;
-            offset =
-                math.min(fixedHeight * items.length, math.max(0.0, offset));
+            offset = math.min(
+                fixedHeight * (items.length - 1), math.max(0.0, offset));
+            print(offset);
           });
         },
-        child: new Container(
+        onVerticalDragEnd: (_) => print("end"),
+        onVerticalDragCancel: () => print("cancel"),
+        onVerticalDragDown: (_) => print("down"),
+        child: new Center(
+//          crossAxisAlignment: CrossAxisAlignment.stretch,
+//          height: 1000.0,
+//        mainAxisSize: MainAxisSize.max,
           child: new Stack(
-            overflow: Overflow.visible,
             fit: StackFit.loose,
             children: items
                 .map((item) {
-                  int index = items.indexOf(item);
-                  double subtract = index * fixedHeight;
-                  double padding = offset - subtract;
-                  padding = math.max(padding, 0.0);
-                  return new Transform.translate(
-                    offset: new Offset(0.0, padding),
+              int index = items.indexOf(item);
+              double subtract = index * fixedHeight;
+              double padding = offset - subtract;
+              padding = math.max(padding, 0.0);
+              return new Transform.translate(
+                offset: new Offset(0.0, padding),
 //                    padding: new EdgeInsets.only(top: padding),
-                    child: new Container(
-                      constraints: new BoxConstraints(minHeight: 400.0),
-                      height: 400.0,
-                      width: 300.0,
-                      child: new FlutterLogo(
+                child: new Container(
+                  constraints: new BoxConstraints(
+                      minHeight: 400.0, maxHeight: 400.0),
+                  height: 400.0,
+                  width: 300.0,
+                  child: new Column(
+                    children: <Widget>[
+                      new Text("$index"),
+                      new FlutterLogo(
                         size: 30.0,
                       ),
-                      decoration: new BoxDecoration(
-                        color: Colors.blue,
-                        border: new Border.all(color: Colors.red, width: 6.0),
-                      ),
-                    ),
-                  );
-                })
+                    ],
+                  ),
+                  decoration: new BoxDecoration(
+                    color: Colors.blue,
+                    border: new Border.all(color: Colors.red, width: 6.0),
+                  ),
+                ),
+              );
+            })
                 .toList()
                 .reversed
                 .toList(),
